be0287e391a489e9c0ebcd546a782de9484ccde3
rlagh2 <4458685+rlagh2@users.noreply.github.com>
2019-05-05 11:32:54 -0700

    Updated

--- CallProcessing/Program.cs
+++ CallProcessing/Program.cs
@@ -1,3 +1,6 @@
+/*
+ * This code is provided "as is" under the MIT license. We provide no warrantees at all.
+ */
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
